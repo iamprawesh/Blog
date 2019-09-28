614cd360c4509de00cb9652ede4477dba737146a
Prawesh <praweshpanthi@gmail.com>
2019-09-28 21:48:12 +0545

    edited wsgi file

--- blog_site/wsgi.py
+++ blog_site/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_site.settings")
 
 application = get_wsgi_application()
 # import os
 # from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 # application = DjangoWhiteNoise(get_wsgi_application())
