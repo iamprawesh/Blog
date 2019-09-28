563133e4902ccfa097f5d52a192fb257a8a6c8fc
Prawesh <praweshpanthi@gmail.com>
2019-09-28 21:45:19 +0545

    added procfile

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+import django-heroku
 # django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
